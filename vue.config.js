c6271a7d9320542634f1ff86f9c59768a739ca41
captainAldi <renaldiyulvianda@yahoo.come>
2019-12-18 23:05:30 +0700

    add config deploy for vue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "transpileDependencies": [
+		"transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/komits-vue/'
+    : '/'
 }
