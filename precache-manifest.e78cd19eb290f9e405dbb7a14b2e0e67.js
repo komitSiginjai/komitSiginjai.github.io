e521e0fa5d8d3884dd9e353201a3be6447c59609
captainAldi <renaldiyulvianda@yahoo.come>
2019-12-21 12:13:18 +0700

    deploy

---
+++ precache-manifest.e78cd19eb290f9e405dbb7a14b2e0e67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63185581f556eda2578b",
+    "url": "/css/about.1722e7c9.css"
+  },
+  {
+    "revision": "824b6760651c3c033061",
+    "url": "/css/app.6182cf67.css"
+  },
+  {
+    "revision": "0c1fc77868f949c30a0a",
+    "url": "/css/chunk-vendors.b963bb4d.css"
+  },
+  {
+    "revision": "37d35ae189611bc125734d731d62c113",
+    "url": "/img/laptop-1.37d35ae1.jpg"
+  },
+  {
+    "revision": "cbd81e55f82a6d23d07777aba06b35b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63185581f556eda2578b",
+    "url": "/js/about.15455e5c.js"
+  },
+  {
+    "revision": "824b6760651c3c033061",
+    "url": "/js/app.35f3b42c.js"
+  },
+  {
+    "revision": "0c1fc77868f949c30a0a",
+    "url": "/js/chunk-vendors.99fd193d.js"
+  },
+  {
+    "revision": "484f8a2509413eb867175d2401bb88cb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
